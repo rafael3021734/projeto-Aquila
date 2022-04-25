f478f2cbc8cdd7661548bdcf29afcbaf15820b9b
Rafael Ferreira <Rafael_santospg@yahoo.com.br>
2022-04-25 14:38:26 -0300

    ajuste start package.json

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const express = require('express');
 const app = express();
  
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 6678);
+app.set('port', process.env.PORT || 3000);
  
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
