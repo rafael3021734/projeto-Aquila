452416a0ac71da2ad7f95a00c18ac28b4c7c1279
Rafael Ferreira <Rafael_santospg@yahoo.com.br>
2022-04-26 00:45:30 -0300

    ajuste start package.json

--- src/swagger.js
+++ src/swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'temple API'
     },
     host: 'apiaquila.herokuapp.com',
-    schemes: ['http']
+    schemes: ['https']
 }
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./src/index.js']
