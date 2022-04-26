29e36d908b7ab7dd92a8db9ac957989100f1b45c
Rafael Ferreira <Rafael_santospg@yahoo.com.br>
2022-04-25 23:30:04 -0300

    ajuste start package.json

--- src/index.js
+++ src/index.js
@@ -77,14 +77,14 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
-});
+});*/
 
 app.get('/usuarios', db.getUsers)
 app.get('/usuario/:id', db.getUserById)
